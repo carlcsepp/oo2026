ec7096f9642c24b2ce5589b69857c8441284a5a2
Carl-Christan Sepp <carlsepp@tlu.ee>
2026-02-17 12:02:39 +0000

    Homework

--- 01_funktsioon/homework.ts
+++ 01_funktsioon/homework.ts
@@ -1,14 +1,14 @@
-// Daily Water Intake
+// Daily Water Intake based on the user's weight
 
 function calculateDWI(weightKg: number): string {
-  const dwi = (weightKg * 35) / 1000;
+  const dwi = (weightKg * 35) / 1000; // 35ml per kg of body weight, a common guideline
   return `Your daily water intake should be approximately ${dwi.toFixed(1)} liters`;
 }
 
 // Example usage
-var weight = 70; // weight in kg
+var weight = 60;
 
 console.log(calculateDWI(weight));
-console.log(calculateDWI(60));
-console.log(calculateDWI(75));
+console.log(calculateDWI(70));
+console.log(calculateDWI(80));
 console.log(calculateDWI(90));
